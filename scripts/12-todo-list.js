9ab31e6e69be8facf8c319eee7e6b5ed2bbd59eb
sanket.ujgare-coditas <sanket.ujgare@coditas.com>
2024-02-23 16:50:41 +0530

    added lesson code

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,71 @@
+const todoList = [{
+    name: 'make dinner',
+    dueDate: '2022-12-22'
+  }, {
+    name: 'wash dishes',
+    dueDate: '2022-12-22'
+  }];
+  
+  renderTodoList();
+  
+  function renderTodoList() {
+    let todoListHTML = '';
+  todoList.forEach((todoObject,index)=>{
+    // const todoObject = todoList[i];
+      //const name = todoObject.name;
+      //const dueDate = todoObject.dueDate;
+      const { name, dueDate } = todoObject;
+      const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button 
+        " class="delete-todo-button js-delete-todo-button">Delete</button> 
+      `;
+      todoListHTML += html;
+  })
+    // for (let i = 0; i < todoList.length; i++) {
+    //   const todoObject = todoList[i];
+    //   //const name = todoObject.name;
+    //   //const dueDate = todoObject.dueDate;
+    //   const { name, dueDate } = todoObject;
+    //   const html = `
+    //     <div>${name}</div>
+    //     <div>${dueDate}</div>
+    //     <button onclick="
+    //       todoList.splice(${i}, 1);
+    //       renderTodoList();
+    //     " class="delete-todo-button">Delete</button> 
+    //   `;
+    //   todoListHTML += html;
+    // }
+  
+    document.querySelector('.js-todo-list')
+      .innerHTML = todoListHTML;
+  }
+  document.querySelector('.js-add-todo-button').addEventListener('click',()=>{
+    addTodo();
+  })
+  document.querySelectorAll('.js-delete-todo-button').forEach((deletebutton,index)=>{
+    deletebutton.addEventListener('click',()=>{
+      todoList.splice(index, 1);
+          renderTodoList();
+    })
+  });
+  function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
+    const name = inputElement.value;
+  
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+  
+    todoList.push({
+      //name: name,
+      //dueDate: dueDate,
+      name,
+      dueDate
+    });
+  
+    inputElement.value = '';
+  
+    renderTodoList();
+  }
